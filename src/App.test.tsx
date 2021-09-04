658c85180939edab9c505f8c35a2f7bc28575f34
Noah Manneschmidt <noah@manneschmidt.net>
2021-09-03 18:50:45 -0700

    Cleanup remaining cruft, update logos

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders the app name", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const name = screen.getByText(/contactful/i);
+  expect(name).toBeInTheDocument();
 });
