cd68920f0c04e7156bab3ad13c4c34e1ea27bdd6
Noah Manneschmidt <noah@manneschmidt.net>
2023-08-22 22:24:24 -0700

    fix local dev

--- local.js
+++ local.js
@@ -1,6 +1,6 @@
 require("dotenv").config({
   path: "./.env.local",
 });
-const app = require("./micro/server");
-app.listen(4000);
+process.env.PORT = 4000;
+require("./micro/server");
 console.log("Listening on port 4000");
